1d22d11689c5dd14abab3ec2d6f3a27e4df849b1
IsaacPCooper <Jordan.P.C.Austin@gmail.com>
2019-10-15 20:42:09 +0100

    chang test script area

--- spec/helpers/enzyme.ts
+++ spec/helpers/enzyme.ts
@@ -2,7 +2,7 @@ import jasmineEnzyme from "jasmine-enzyme";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() });
+configure({ adapter: Adapter() });
 
 beforeEach(function() {
   jasmineEnzyme();
--- spec/ongoing.js
+++
@@ -1,9 +0,0 @@
-import App from "./App";
-import ONGOING_GAME from "./App";
-
-describe("Game Is In Progress", () => {
-  it("should show when a game is ongoing", () => {
-    const ongoingcheck = App(ONGOING_GAME);
-    expect(ongoingcheck).toBe(-1);
-  });
-});
