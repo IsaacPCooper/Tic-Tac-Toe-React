4d3fad0211d623494c8e360d4c4df8aa6e88acde
IsaacPCooper <Jordan.P.C.Austin@gmail.com>
2019-10-15 20:05:19 +0100

    jasmine changes?

--- spec/ongoing.js
+++ spec/ongoing.js
@@ -1,6 +1,9 @@
+import App from "./App";
+import ONGOING_GAME from "./App";
+
 describe("Game Is In Progress", () => {
   it("should show when a game is ongoing", () => {
-    const ongoingcheck = ONGOING_GAME;
+    const ongoingcheck = App(ONGOING_GAME);
     expect(ongoingcheck).toBe(-1);
   });
 });
