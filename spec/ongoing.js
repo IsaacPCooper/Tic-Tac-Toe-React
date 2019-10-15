80190d0101cf2a60bace92fda5cbd5c0e1ba73bc
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2019-10-15 19:45:49 +0100

    Create ongoing.js

---
+++ spec/ongoing.js
@@ -0,0 +1,6 @@
+describe('Game Is In Progress', () => {
+  it('should show when a game is ongoing', () => {
+      const ongoingcheck =  ONGOING_GAME;
+      expect(ongoingcheck).toBe(-1);
+  })
+})
